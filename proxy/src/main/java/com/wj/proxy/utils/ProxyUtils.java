3844344643838903c3f4ddae5f6d26ac4f7b5639
wangjia <wangjia_yql@qq.com>
2018-02-04 21:47:26 +0800

    ProxyUtils

---
+++ proxy/src/main/java/com/wj/proxy/utils/ProxyUtils.java
@@ -0,0 +1,25 @@
+package com.wj.proxy.utils;
+
+import sun.misc.ProxyGenerator;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class ProxyUtils {
+
+    public static void generateClassFile(Class clazz, String proxyName) {
+        byte[] classFile = ProxyGenerator.generateProxyClass(proxyName, clazz.getInterfaces());
+        String paths = "/Users/wangjia/";
+        System.out.println(paths);
+//        OutputStream out = null;
+        try (OutputStream out = new FileOutputStream(paths + proxyName + ".class");) {
+            //保留到硬盘中
+            out.write(classFile);
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
